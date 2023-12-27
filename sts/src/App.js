7148061b5f89da3b733e679e33ea4ae549c2fa94
Sven <108429493+svenfydrich@users.noreply.github.com>
2023-12-27 18:15:51 +0100

    fixed css and started adding media query for responsive design

--- sts/src/App.js
+++ sts/src/App.js
@@ -1,30 +1,49 @@
 import './App.css';
 import chatbotImage from './assets/chatbot.png';
-import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-
-        <div class='navbarwrapper'>
-        <h1 class='logo'>ShowtimeSherlock</h1>
-          <ul class='navbarlist'>
-            <li id='about'>
-              <a href="#aboutus" class="aboutus">Über uns</a>
+        <div className="navbarwrapper">
+          <h1 className="logo">ShowtimeSherlock</h1>
+          <ul className="navbarlist">
+            <li id="about">
+              <a href="#aboutus" className="aboutus">
+                Über uns
+              </a>
             </li>
-            <li id='history'>
-              <a href='#history' class="history">Geschichte</a>
+            <li id="history">
+              <a href="#history" className="history">
+                Geschichte
+              </a>
             </li>
-            <li id='test'><button id='testbutton'><a href="url" class="test">TESTEN</a></button></li>
+            <li id="test">
+              <button id="testbutton">
+                <a href="url" className="test">
+                  TESTEN
+                </a>
+              </button>
+            </li>
           </ul>
         </div>
-        
-        <div class='textbox'>
-          <h1 class='headline'>Der schnellste Eventvermittler.</h1>
-          <h2 class="subtitle">Finde in Echtzeit aktuelle Veranstaltungen, von Konzerten bis zu Kunstausstellungen.</h2>
-          <button id="testnow"><a href="url" class="testnow">Jetzt testen</a> </button>
-          <img src={chatbotImage} alt="Hier ist der Chatbot zu erkennen." id="chatbotimg"/>
+
+        <div className="textbox">
+          <div className="text-content">
+            <h1 className="headline">Der schnellste Eventvermittler.</h1>
+            <h2 className="subtitle">
+              Finde in Echtzeit aktuelle Veranstaltungen, von Konzerten bis zu
+              Kunstausstellungen.
+            </h2>
+            <button id="testnow">
+              <a href="url" className="testnow">
+                Jetzt testen
+              </a>{' '}
+            </button>
+          </div>
+          <div className="image-container">
+            <img src={chatbotImage} alt="Hier ist der Chatbot zu erkennen." id="chatbotimg" />
+          </div>
         </div>
       </header>
     </div>
